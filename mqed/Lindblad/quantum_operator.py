7c30f4686fc0d9eb07d54a248e44d63398465645
Guangming1998 <gliu8@nd.edu>
2025-10-20 17:07:11 -0400

    Implement a new quantum operator: excited state population operator.

--- mqed/Lindblad/quantum_operator.py
+++ mqed/Lindblad/quantum_operator.py
@@ -1,4 +1,4 @@
-from qutip import Qobj, qeye
+from qutip import Qobj, qeye, projection
 import numpy as np
 
 
@@ -14,3 +14,18 @@ def msd_operator(dim: int, d_nm: float, Nmol: int, init_site_index: int) -> Qobj
     X = Qobj(np.diag(positions), dims=[[dim], [dim]])
     x0 = positions[init_site_index]
     return (X - x0 * qeye(dim)) ** 2
+
+def excitation_population_operator(dim: int, Nmol:int) -> Qobj:
+    """
+    This creates a list of projectors:
+    |e_j><e_j| for j=1,...,Nmol
+    where |e_j> is the state with excitation on molecule j.
+    Args:
+        dim (int): Dimension of the Hilbert space (Nmol + 1).
+        Nmol (int): Number of molecules.
+    Returns:
+        List[Qobj]: List of projection operators for each molecule's excitation.
+    """
+    e_ops_populations = [projection(dim, i+1, i+1) for i in range(Nmol)]
+
+    return e_ops_populations
